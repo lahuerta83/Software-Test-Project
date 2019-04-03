529c189475781bf9b14523be8e01a081a2441683
Adam Murr <adamcmurr@gmail.com>
2019-04-02 21:08:43 -0400

    Added Click Events
    The buttons were not mapped to the click events.

--- ProjectDogGroomer/Form1.Designer.cs
+++ ProjectDogGroomer/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace ProjectDogGroomer
+﻿    namespace ProjectDogGroomer
 {
     partial class Form1
     {
@@ -30,13 +30,13 @@
         {
             this.adddog = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.dogname = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.nextdog = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.nextdog = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dogname = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.line = new System.Windows.Forms.ListBox();
             this.groupBox1.SuspendLayout();
@@ -51,6 +51,7 @@
             this.adddog.TabIndex = 0;
             this.adddog.Text = "Add Dog Button";
             this.adddog.UseVisualStyleBackColor = false;
+            this.adddog.Click += new System.EventHandler(this.adddog_Click);
             // 
             // groupBox1
             // 
@@ -66,58 +67,6 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Services";
             // 
-            // dogname
-            // 
-            this.dogname.Location = new System.Drawing.Point(174, 53);
-            this.dogname.Name = "dogname";
-            this.dogname.Size = new System.Drawing.Size(216, 20);
-            this.dogname.TabIndex = 2;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(68, 171);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(156, 85);
-            this.textBox2.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(59, 53);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Dog\'s Name";
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(100, 89);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(67, 17);
-            this.checkBox1.TabIndex = 1;
-            this.checkBox1.Text = "Nail Trim";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(68, 128);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(156, 27);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Add Services";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // nextdog
-            // 
-            this.nextdog.Location = new System.Drawing.Point(68, 275);
-            this.nextdog.Name = "nextdog";
-            this.nextdog.Size = new System.Drawing.Size(156, 27);
-            this.nextdog.TabIndex = 3;
-            this.nextdog.Text = "Next Dog";
-            this.nextdog.UseVisualStyleBackColor = true;
-            // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
@@ -130,6 +79,60 @@
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(176, 21);
             this.comboBox1.TabIndex = 4;
+            // 
+            // nextdog
+            // 
+            this.nextdog.Location = new System.Drawing.Point(68, 275);
+            this.nextdog.Name = "nextdog";
+            this.nextdog.Size = new System.Drawing.Size(156, 27);
+            this.nextdog.TabIndex = 3;
+            this.nextdog.Text = "Next Dog";
+            this.nextdog.UseVisualStyleBackColor = true;
+            this.nextdog.Click += new System.EventHandler(this.nextdog_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(68, 128);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 27);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Add Services";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(100, 89);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(67, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Nail Trim";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(68, 171);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(156, 85);
+            this.textBox2.TabIndex = 0;
+            // 
+            // dogname
+            // 
+            this.dogname.Location = new System.Drawing.Point(174, 53);
+            this.dogname.Name = "dogname";
+            this.dogname.Size = new System.Drawing.Size(216, 20);
+            this.dogname.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Dog\'s Name";
             // 
             // label2
             // 
